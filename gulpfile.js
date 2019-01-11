fa18677c8fc5913801d05f9f2b4aff01b0e179e2
Michael Holleran <michael@bravewhale.com>
2019-01-11 00:20:56 -0800

    added gulp zip, updated docker-compose php configs

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,6 @@
+/* eslint-env node */
+/* eslint no-unused-vars: 1 */
+
 // create docs
 // generate classes, hooks
 // test
@@ -5,6 +8,16 @@ const gulp = require('gulp');
 const _ = require('lodash');
 const argv = require('yargs').argv;
 const fs = require('fs');
+const source = require('env-file-reader').parse;
+const archiver = new require('archiver');
+
+let local = source('./.env');
+
+/****** setup ******/
+gulp.task('init', () => {
+	console.log('require slug and nicename of plugin');
+});
+
 
 /****** generating ******/
 // generate post types (post|page|RESTful)
@@ -21,8 +34,32 @@ gulp.task('reset', () => {});
 gulp.task('capture', () => {});
 
 /****** testing ******/
-// run API tests
+// run js API tests
+gulp.task('test')
+// run selenium tests
 
 /****** packaging ******/
+// replace all __PLUGIN_NAME__ in plugin dir
 // create zip
-// generate docs
+gulp.task('zip', (done) => {
+  let output = fs.createWriteStream('./' + local.PLUGIN_NAME + '.zip');
+  let archive = archiver('zip', {
+    zlib: { level: 9 } 
+  });
+  
+  output.on('close', function() {
+    console.log(archive.pointer() + ' total bytes');
+    console.log('archiver has been finalized and the output file descriptor has closed.');
+    done();
+  });
+  
+  archive.on('error', function(err) {
+    throw err;
+  });
+  archive.pipe(output);
+
+  archive.directory('plugin', local.PLUGIN_NAME);
+  archive.finalize();
+});
+// generate jsdocs
+// generate phpdocs
